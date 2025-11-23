78172276e205b0c72f7febef125fef46b7ca461a
Aryan <94785855+JAIMNBIDU@users.noreply.github.com>
2025-11-24 00:27:20 +0530

    Add check for empty input in longestCommonPrefix

--- Code/longest_common_prefix.py
+++ Code/longest_common_prefix.py
@@ -3,6 +3,7 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
+            
         prefix = strs[0]
 
         for s in strs[1:]:
