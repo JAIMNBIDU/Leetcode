ef1d386445c49dd8dec7785e98e7291ee466d2c1
Aryan <94785855+JAIMNBIDU@users.noreply.github.com>
2025-11-24 00:26:08 +0530

    Clean up formatting in longest_common_prefix.py
    Remove unnecessary blank line in longest_common_prefix.py

--- Code/longest_common_prefix.py
+++ Code/longest_common_prefix.py
@@ -1,10 +1,8 @@
 from typing import List
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        
         prefix = strs[0]
 
         for s in strs[1:]:
